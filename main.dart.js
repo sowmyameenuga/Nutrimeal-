7485f1fd3c35ab96c453715296a41677ce5738c3
sowmyameenuga <sowmyameenuga@users.noreply.github.com>
2026-06-23 03:43:38 +0000

    deploy: 2f7949e858ed93ebff12c3391522a568cea7f75f

--- main.dart.js
+++ main.dart.js
@@ -28989,7 +28989,7 @@ $S:2}
 A.awL.prototype={
 $1(a){var s=A.df().b
 s=s==null?null:s.canvasKitBaseUrl
-return(s==null?"https://www.gstatic.com/flutter-canvaskit/77e2e94772b6eb43759e34ed1ad7da4674e19cab/":s)+a},
+return(s==null?"https://www.gstatic.com/flutter-canvaskit/a4ce257c68517c1410f4b48ac9852ab5642a3f8d/":s)+a},
 $S:106}
 A.MZ.prototype={
 gA(a){var s=this.a
